beaa9c25c32f49b191773ea249cecf3beea99b08
alissonfragoso <alissonfragoso123@gmail.com>
2024-02-28 21:53:18 -0300

    listar e deletar usuario

--- routes/rotaUsuario.js
+++ routes/rotaUsuario.js
@@ -1,85 +1,119 @@
 const express = require("express");
 const router = express.Router();
 const sqlite3 = require("sqlite3").verbose();
-
-const usuario =[
+const db = new sqlite3.Database("database.db");
+const usuario = [
     {
-    id:1,
-    nome:"Bleno",
-    email:"bleno@gmail.com",
-    senha:"123",
-
-    },
-    {
-    id:2,
-    nome:"Felipe",
-    email:"felipe@gmail.com",
-    senha:"123",
+        id: 1,
+        nome: "Bleno",
+        email: "bleno@gmail.com",
+        senha: "123",
 
     },
     {
-    id:3,
-    nome:"Alisson",
-    email:"alisson@gmail.com",
-    senha:"123",
+        id: 2,
+        nome: "Felipe",
+        email: "felipe@gmail.com",
+        senha: "123",
 
     },
     {
-    id:4,
-    nome:"Carlos",
-    email:"Cerlos@gmail.com",
-    senha:"123",
+        id: 3,
+        nome: "Alisson",
+        email: "alisson@gmail.com",
+        senha: "123",
+
+    },
+    {
+        id: 4,
+        nome: "Carlos",
+        email: "Cerlos@gmail.com",
+        senha: "123",
 
     },
 ]
 
 
-router.get("/",(req,res,next)=>{
+router.get("/", (req, res, next) => {
+
+    db.all("SELECT * FROM usuario", (error,rows)=>{
+        if(error){
+           return res.status(500).send({
+            error:error.message
+           })
+        }
+        res.status(200).send({
+            messagem:"Aqui está a lista de Usuários",
+            usuarios:rows   
+        })
+    })
     
-    res.json(usuario)
 });
 
-router.get("/nomes",(req,res,next)=>{
-    let nomes=[];
-    usuario.map((linha)=>{
+router.get("/nomes", (req, res, next) => {
+    let nomes = [];
+    usuario.map((linha) => {
         nomes.push({
-            nome:linha.nome,
-            email:linha.email
+            nome: linha.nome,
+            email: linha.email
         })
     })
     res.json(nomes)
 });
 
-router.post("/",(req,res,next) => {
-    const db = new sqlite3.Database("database.db")
+router.post("/", (req, res, next) => {
+   
 
-    const {nome,email,senha} = req.body;
+    const { nome, email, senha } = req.body;
 
-    db.serialize(()=>{
-      
-        db.run("CREATE TABLE IF NOT EXISTS usuario(id INTEGER PRIMARY KEY AUTOINCREMENT,nome TEXT email TEXT UNIQUE,  senha TEXT)")
+    db.serialize(() => {
+
+        db.run("CREATE TABLE IF NOT EXISTS usuario(id INTEGER PRIMARY KEY AUTOINCREMENT,nome TEXT, email TEXT UNIQUE,  senha TEXT)")
         const insertUsuario = db.prepare("INSERT INTO usuario(nome,email,senha) VALUES (?,?,?)")
-        insertUsuario.run(nome,email,senha);
+        insertUsuario.run(nome, email, senha);
         insertUsuario.finalize()
+
     })
 
+    process.on("SIGINT", () => {
 
-    res.status(200).send({mensagem:"Salvo com Sucesso!!!!!!!"});              
+        db.close((err) => {
+            if (err) {
+                return res.status(304).send(err.message);
+ 
+            }
+        })
+
+    })
+
+    res.status(200).send({ mensagem: "Salvo com Sucesso!!!!!!!" });
 
 })
 
-router.put("/",(req,res,next)=>{
+router.put("/", (req, res, next) => {
 
     const id = req.body.id;
 
-    res.send({id:id});              
+    res.send({ id: id });
 
 })
-router.delete("/:id",(req,res,next)=>{
+router.delete("/:id", (req, res, next) => {
 
-    const id = req.params;
+    const {id} = req.params;
+    db.run("DELETE  FROM usuario WHERE  id = ?",id, (error,)=>{
 
-    res.send({id:id});              
+        if(error){
+            return res.status(500).send({
+             error:error.message
+            })
+         }
+         res.status(200).send({
+            messagem:"Cadastros deletado com suscesso!!",
+           
+        })
+    });
+
+    
 
 })
 module.exports = router;
